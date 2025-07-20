aec12e94b1a37a01e867d87a04274c8ec235c22e
Rob Scott <robscott460@gmail.com>
2025-07-20 20:17:10 +0100

    feat: add login command and command handling

--- main.go
+++ main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/varedis/gator-cli/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
-	fmt.Printf("Read config %+v\n", cfg)
 
-	if err = cfg.SetUser("Rob"); err != nil {
-		log.Fatalf("couldn't set the current user: %v", err)
+	programState := &state{
+		cfg: &cfg,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("Error loading config: %v", err)
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
-	fmt.Printf("Read config again %+v\n", cfg)
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	if err := cmds.run(programState, command{Name: cmdName, Args: cmdArgs}); err != nil {
+		log.Fatal(err)
+	}
 }
